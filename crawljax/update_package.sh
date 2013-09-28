eb71b5ee94dd32147d9bffd82ea777b6effc3c32
grimsock <wojciech.ochmanski@gmail.com>
2013-09-28 15:16:17 +0200

    [crawljax] update package script

---
+++ crawljax/update_package.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+if [[ $# == 0 ]] || [[ $# > 1 ]]
+then
+  echo "invalid numer of args, pass just package version" && exit 1
+fi
+
+new_version=$1
+current_version=`grep 'pkgver=' PKGBUILD | grep -o '[0-9.]\+'`
+
+# replace current version with new one
+sed -i -e "/pkgver=/ s/$current_version/$new_version/g" PKGBUILD
+
+# update checksums
+updpkgsums PKGBUILD
+
+# if namcap installed perform chceck
+if (pacman -Q namcap &>/dev/null); then
+  echo "==============================="
+  echo "performing namcap check"
+  namcap -i PKGBUILD
+  echo "==============================="
+fi
+
+# generate package
+makepkg --source --force --clean
+
+# cleanup
+rm -rf *.zip
+
+echo "ready to AUR upload"
