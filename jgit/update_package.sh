61fce249faefa61762a1e3d3dac351b98337bca7
grimsock <wojciech.ochmanski@gmail.com>
2013-09-28 16:08:01 +0200

    [jgit] update package script

---
+++ jgit/update_package.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+
+if [[ $# == 0 ]] || [[ $# > 1 ]]
+then
+  echo "invalid numer of args, pass just package version" && exit 1
+fi
+
+# pkgver can't have whitespaces, colons, dashes
+new_version=${1//-/_}
+
+current_version=`grep 'pkgver=' PKGBUILD | grep -o '[0-9._]\+r'`
+
+# replace current version with new one
+sed -i -e "/pkgver=/ s/$current_version/$new_version/g" PKGBUILD
+
+# update checksums
+updpkgsums PKGBUILD
+
+# if namcap installed perform chceck
+if (pacman -Q namcap &>/dev/null); then
+  echo "==============================="
+  echo "performing namcap check"
+  namcap -i PKGBUILD
+  echo "==============================="
+fi
+
+# generate package
+makepkg --source --force --clean
+
+# cleanup
+rm -rf *.zip
+rm -rf *"$1".sh
+
+echo "ready to AUR upload"
